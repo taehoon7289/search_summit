5640ef32d5ba7da9ad10194be743417163f6018b
taehoon7289 <taehoon7289@gmail.com>
2022-10-25 18:34:42 +0900

    feat: 카메라 코드 작업중

--- app/src/main/java/com/minikode/summit/ui/MainActivity.kt
+++ app/src/main/java/com/minikode/summit/ui/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.minikode.summit.ui
 
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.google.common.util.concurrent.ListenableFuture
 import com.minikode.summit.BaseActivity
 import com.minikode.summit.R
 import com.minikode.summit.databinding.ActivityMainBinding
@@ -7,10 +14,35 @@ import com.minikode.summit.databinding.ActivityMainBinding
 class MainActivity : BaseActivity<ActivityMainBinding>() {
     override val layoutRes: Int = R.layout.activity_main
 
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+
     override fun initView() {
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider = cameraProviderFuture.get()
+            bindPreview(cameraProvider)
+        }, ContextCompat.getMainExecutor(this))
+
         with(binding) {
-            textView.text = "ddd gogogo"
         }
+    }
+
+    fun bindPreview(cameraProvider: ProcessCameraProvider) {
+        var preview: Preview = Preview.Builder()
+            .build()
+
+        var cameraSelector: CameraSelector = CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+            .build()
+
+        preview.setSurfaceProvider(binding.previewView.surfaceProvider)
+
+        var camera = cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
+
+        binding.previewView.implementationMode = PreviewView.ImplementationMode.COMPATIBLE
+        binding.previewView.scaleType = PreviewView.ScaleType.FILL_CENTER
+
     }
 
 }
